8134622bb3377244b614ee2f021f4f4265faf8b7
Sujeong Ji <suji.sujeongji@gmail.com>
2022-09-17 00:54:00 +0900

    Feat : Input 컴포넌트 props 추가 (#10)

--- client/components/Input/Input.tsx
+++ client/components/Input/Input.tsx
@@ -1,15 +1,16 @@
-import {colors} from '@/styles/colors'
 import React, {HTMLInputTypeAttribute} from 'react'
 import styled from 'styled-components'
+import {colors} from '@/styles/colors'
 
 export interface InputProps {
   type: HTMLInputTypeAttribute
   name: string
   placeholder?: string
-  value: string
-  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+  value?: string
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void
   width?: string
   height?: string
+  marginBottom?: string
 }
 
 function Input({type, placeholder, value, onChange, ...props}: InputProps) {
@@ -30,7 +31,18 @@ const StyleInput = styled.input<InputProps>`
   padding-left: 1rem;
   width: ${({width}) => width ?? '30rem'};
   height: ${({height}) => height ?? '3rem'};
+  margin-bottom: ${({marginBottom}) => marginBottom ?? '0'};
   border-radius: 3px;
   background-color: ${colors.grey1};
   border: none;
+
+  ::placeholder {
+    color: ${colors.grey2};
+    letter-spacing: 0.05rem;
+  }
+
+  :focus {
+    border: 0.05rem solid ${colors.mainColor};
+    outline: 0.4rem solid rgb(231, 245, 232);
+  }
 `
