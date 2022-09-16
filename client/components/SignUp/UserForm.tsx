a42196a6db8582fb87af853a72597d74e6cc5796
JEONGHWANMIN <mimi0j0j@naver.com>
2022-09-16 22:45:01 +0900

    Feat : 회원가입 레이아웃 및 유효성 로직 추가 (#4)

--- client/components/SignUp/UserForm.tsx
+++ client/components/SignUp/UserForm.tsx
@@ -1,23 +1,108 @@
 import {Form} from '@/pages/signup'
-import {colors} from '@/styles/colors'
-import React from 'react'
+import {Validate} from '@/utils/validate'
+import React, {useState} from 'react'
 import styled from 'styled-components'
 import AuthButton from '../AuthButton/AuthButton'
-import CheckBox from '../CheckBox/CheckBox'
 import LabelInput from '../LabelInput/LabelInput'
-import CheckBoxLabel from './CheckBoxLabel'
+import AgreeBox from './AgreeBox'
 import Postcode from './Postcode'
 
 interface Props {
-  form: Form
-  setForm: React.Dispatch<React.SetStateAction<Form>>
-  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
-  onSubmit: (e: React.FormEvent<HTMLFormElement>) => void
   mode: 'user' | 'company'
 }
 
-function UserForm({form, onChange, mode, setForm, onSubmit}: Props) {
+function UserForm({mode}: Props) {
+  const [form, setForm] = useState({
+    email: '',
+    password: '',
+    passwordCheck: '',
+    name: '',
+    phone: '',
+    companyName: '',
+    address: '',
+    detailAddress: '',
+  })
+
+  const [errors, setErrors] = useState({
+    email: true,
+    password: true,
+    passwordCheck: true,
+    phone: true,
+  })
+
+  const [checks, setChecks] = useState({
+    totalAgree: false,
+    useAgree: false,
+    privateAgree: false,
+    snsAgree: false,
+    ageAgree: false,
+  })
+
   const {email, password, passwordCheck, name, phone, companyName, detailAddress, address} = form
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    let error = false
+    const {name, value} = e.target
+    setForm({
+      ...form,
+      [name]: value,
+    })
+    if (name === 'email') {
+      error = Validate.emailValidate(value)
+    }
+
+    if (name === 'password') {
+      error = Validate.passwordValidate(value)
+    }
+
+    if (name === 'passwordCheck') {
+      error = Validate.passwordConfirmValidate(password, value)
+    }
+
+    if (name === 'phone') {
+      error = Validate.phoneValidate(value)
+    }
+
+    setErrors({
+      ...errors,
+      [name]: error,
+    })
+  }
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const newForm: Partial<Form> = form
+
+    if (mode === 'user') {
+      delete newForm.companyName
+      delete newForm.address
+      delete newForm.detailAddress
+    }
+
+    const {email, password, passwordCheck, name, phone} = newForm
+
+    if (!email || !password || !passwordCheck || !name || !phone) {
+      alert('모든 항목을 입력해주세요')
+      return
+    }
+
+    if (
+      errors.email === true ||
+      errors.password === true ||
+      errors.passwordCheck === true ||
+      errors.phone === true
+    ) {
+      alert('입력값을 확인해주세요')
+      return
+    }
+
+    if (!checks.useAgree || !checks.privateAgree || !checks.ageAgree) {
+      alert('필수 항목에 동의해주세요')
+      return
+    }
+
+    console.log(newForm)
+  }
 
   return (
     <SignUpForm onSubmit={onSubmit}>
@@ -27,6 +112,8 @@ function UserForm({form, onChange, mode, setForm, onSubmit}: Props) {
         value={email}
         onChange={onChange}
         label={'이메일'}
+        Errors={errors.email}
+        ErrorMessage={'이메일 형식이 올바르지 않습니다.'}
       />
       <LabelInput
         type={'password'}
@@ -34,6 +121,8 @@ function UserForm({form, onChange, mode, setForm, onSubmit}: Props) {
         value={password}
         onChange={onChange}
         label={'비밀번호'}
+        Errors={errors.password}
+        ErrorMessage={'비밀번호는 문자 숫자 특수문자 조합 8자 이상으로 조합해주세요.'}
       />
       <LabelInput
         type={'password'}
@@ -41,14 +130,18 @@ function UserForm({form, onChange, mode, setForm, onSubmit}: Props) {
         value={passwordCheck}
         onChange={onChange}
         label={'비밀번호 확인'}
+        Errors={errors.passwordCheck}
+        ErrorMessage={'비밀번호가 일치하지 않습니다. 다시 확인해 주세요.'}
       />
       <LabelInput type={'text'} name={'name'} value={name} onChange={onChange} label={'이름'} />
       <LabelInput
-        type={'number'}
+        type={'text'}
         name={'phone'}
         value={phone}
         onChange={onChange}
         label={'전화번호'}
+        Errors={errors.phone}
+        ErrorMessage={'핸드폰 번호 형식이 올바르지 않습니다.'}
       />
       {mode === 'company' && (
         <>
@@ -79,6 +172,8 @@ function UserForm({form, onChange, mode, setForm, onSubmit}: Props) {
           />
         </>
       )}
+      <Border />
+      <AgreeBox checks={checks} setChecks={setChecks} />
       <AuthButton title="회원가입" />
     </SignUpForm>
   )
@@ -95,4 +190,11 @@ const SignUpForm = styled.form`
 const AddressBox = styled.div`
   display: flex;
   gap: 7px;
+`
+const Border = styled.div`
+  margin: 40px 0;
+  width: 100%;
+  height: 0.1px;
+  border: 0.5px solid #e5e5e5;
+  background-color: #e5e5e5;
 `
