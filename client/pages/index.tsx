2bb33ab0c1e898f58f60a53ddc66a19d0f5f0dcc
JEONGHWANMIN <mimi0j0j@naver.com>
2022-09-21 02:20:21 +0900

    Feat : 메인 홈 페이지 레이아웃 구성 (#29)

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -2,9 +2,13 @@ import Image from 'next/image'
 import {ToastContainer} from 'react-toastify'
 import styled from 'styled-components'
 import mainBanner from '@/public/images/mainBanner.png'
+import subBanner from '@/public/images/subBanner.jpg'
 import CategoryTag from '@/components/Home/CategoryTag'
 import {colors} from '@/styles/colors'
 import {categoryTags} from '@/utils/options/options'
+import PostCard from '@/components/Home/PostCard'
+import {dummyPosts} from '@/utils/dummy/dummy'
+import SearchBar from '@/components/Home/SearchBar'
 
 function Home() {
   return (
@@ -12,12 +16,27 @@ function Home() {
       <MainBanner>
         <Image src={mainBanner} />
       </MainBanner>
-      <MainSearchBar></MainSearchBar>
+      <MainSearchBar>
+        <SearchBar />
+      </MainSearchBar>
       <CategoryBox>
-        {categoryTags.map((title) => (
-          <CategoryTag title={title} />
+        {categoryTags.map((title, idx) => (
+          <CategoryTag title={title} key={idx} />
         ))}
       </CategoryBox>
+      <PostCardBox>
+        {Array.from({length: 8}).map((_, idx) => (
+          <PostCard key={idx} post={dummyPosts[0]}></PostCard>
+        ))}
+      </PostCardBox>
+      <SubBanner>
+        <Image src={subBanner} />
+      </SubBanner>
+      <PostCardBox>
+        {Array.from({length: 8}).map((_, idx) => (
+          <PostCard key={idx} post={dummyPosts[0]}></PostCard>
+        ))}
+      </PostCardBox>
       <ToastContainer />
     </Container>
   )
@@ -35,10 +54,10 @@ const MainBanner = styled.div`
 
 const MainSearchBar = styled.div`
   width: 100%;
-  height: 150px;
+  height: 100px;
+  padding-top: 2rem;
+  padding-bottom: 2rem;
   background-color: ${colors.grey1};
-
-  margin-bottom: 40px;
 `
 
 const CategoryBox = styled.div`
@@ -47,12 +66,13 @@ const CategoryBox = styled.div`
   flex-wrap: wrap;
   gap: 30px;
   width: 100%;
-  height: 107px;
+  margin: 40px 0;
 
   & > div:nth-child(odd) {
     background-color: ${colors.subColor1};
     color: ${colors.subFontColor1};
   }
+
   & > div:nth-child(even) {
     background-color: ${colors.subColor2};
     color: ${colors.subFontColor2};
@@ -65,4 +85,16 @@ const CategoryBox = styled.div`
   @media (max-width: 390px) {
     gap: 15px;
   }
+`
+
+const PostCardBox = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+  gap: 15px;
+`
+
+const SubBanner = styled.div`
+  margin-bottom: 50px;
 `
