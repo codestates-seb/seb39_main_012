f8590fdfe90254f546c23bc77ec0a411eb38d99c
Sujeong Ji <suji.sujeongji@gmail.com>
2022-09-21 02:26:01 +0900

    Design : 전역 폰트 추가 (#19)

--- client/styles/GlobalStyle.ts
+++ client/styles/GlobalStyle.ts
@@ -6,6 +6,8 @@ export const GlobalStyle = createGlobalStyle`
   html {
     font-size: 62.5%;
     box-sizing: border-box;
+    font-family: 나눔스퀘어, 'NanumSquare', sans-serif;
+    color: rgb(51, 51, 51);
   }
   
 `
