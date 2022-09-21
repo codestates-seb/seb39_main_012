ff4d8fb02d3899ce19ffb6822f7efa9f1d3625ef
penrose15 <seongju99@naver.com>
2022-09-21 23:08:27 +0900

    feat : @param 추가

--- server2/src/main/java/com/team012/server/company/room/repository/RoomRepository.java
+++ server2/src/main/java/com/team012/server/company/room/repository/RoomRepository.java
@@ -3,15 +3,16 @@ package com.team012.server.company.room.repository;
 import com.team012.server.company.room.entity.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface RoomRepository extends JpaRepository<Room, Long> {
     @Query("select r from Room r where r.postsId = :id")
-    List<Room> findAllByPostsId(Long id);
+    List<Room> findAllByPostsId(@Param("id") Long id);
 
     List<Room> findByPostsId(Long postsId);
 
     @Query("select min(r.price) from Room r where r.postsId = :id")
-    Integer findMinPrice(Long id);
+    Integer findMinPrice(@Param("id")Long id);
 }
--- server2/src/main/java/com/team012/server/posts/img/repository/PostsImgRepository.java
+++ server2/src/main/java/com/team012/server/posts/img/repository/PostsImgRepository.java
@@ -3,10 +3,11 @@ package com.team012.server.posts.img.repository;
 import com.team012.server.posts.img.entity.PostsImg;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface PostsImgRepository extends JpaRepository<PostsImg, Long> {
     @Query("select i from PostsImg i where i.posts.id = :id")
-    List<PostsImg> findAllByPostsId(Long id);
+    List<PostsImg> findAllByPostsId(@Param("id")Long id);
 }
