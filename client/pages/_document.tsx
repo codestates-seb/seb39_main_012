45fe68d9296c0ae1d6a9c71747aaa7929ab1844b
Sujeong Ji <suji.sujeongji@gmail.com>
2022-09-30 00:33:28 +0900

    Design : 폰트 추가 (#19)

--- client/pages/_document.tsx
+++ client/pages/_document.tsx
@@ -5,6 +5,16 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+          <link rel="preconnect" href="https://fonts.gstatic.com"></link>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Nanum+Pen+Script&display=swap"
+            rel="stylesheet"
+          ></link>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
+            rel="stylesheet"
+          ></link>
           <link
             rel="stylesheet"
             type="text/css"
