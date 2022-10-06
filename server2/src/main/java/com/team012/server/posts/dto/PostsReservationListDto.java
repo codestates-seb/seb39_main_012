6d973c0cfe57dd5f0dea69d5440675db8e8cf8a0
penrose15 <seongju99@naver.com>
2022-10-06 09:34:03 +0900

    fix : 다녀온 내역 API postID 추가

--- server2/src/main/java/com/team012/server/posts/dto/PostsReservationListDto.java
+++ server2/src/main/java/com/team012/server/posts/dto/PostsReservationListDto.java
@@ -3,16 +3,39 @@ package com.team012.server.posts.dto;
 import lombok.Builder;
 import lombok.Getter;
 
-@Getter
 public class PostsReservationListDto {
-    private String title;
-    private Integer roomPrice;
-    private String url;
 
-    @Builder
-    public PostsReservationListDto(String title, Integer roomPrice, String url) {
-        this.title = title;
-        this.roomPrice = roomPrice;
-        this.url = url;
+    @Getter
+    public static class BookedList {
+        private String title;
+        private Integer roomPrice;
+        private String url;
+
+        @Builder
+        public BookedList(String title, Integer roomPrice, String url) {
+            this.title = title;
+            this.roomPrice = roomPrice;
+            this.url = url;
+        }
     }
+
+    @Getter
+    public static class BookedListAfterCheckOut {
+        private Long postsId;
+        private String title;
+        private Integer roomPrice;
+        private String url;
+
+        @Builder
+        public BookedListAfterCheckOut(Long postsId, String title, Integer roomPrice, String url) {
+            this.postsId = postsId;
+            this.title = title;
+            this.roomPrice = roomPrice;
+            this.url = url;
+        }
+    }
+
+
+
+
 }
