4f3f7e6f103d3149275a3dac7b9fe5d86ff7590b
Sujeong Ji <suji.sujeongji@gmail.com>
2022-10-07 16:53:50 +0900

    Feat : 옵션 카테고리 추가 (#51)

--- client/utils/options/options.ts
+++ client/utils/options/options.ts
@@ -3,7 +3,6 @@ export const categoryTags = [
   '소형견',
   '중형견',
   '대형견',
-  '데이케어',
   '노견케어',
   '미용',
   '산책',
@@ -16,17 +15,19 @@ export const dogSizeOption = [
   {label: '대형견', value: 'large'},
 ]
 
+export const availableTags = ['소형견', '중형견', '대형견', '노견케어', '미용', '산책', '훈련']
+
 export const availableServices = [
-  ['소형견 케어', '7kg 미만'],
-  ['중형견 케어', '7kg 이상 14.9kg 미만'],
-  ['대형견 케어', '15kg 이상'],
-  ['노견 케어', '8살 이상 노견 돌봄'],
-  ['산책 & 실외 배변', '1일 2회 산책'],
-  ['미용', '컷별 미용 가능'],
-  ['목욕', '목욕 케어 가능'],
-  ['실내 놀이', '터그놀이, 노즈워크 등'],
-  ['CCTV', '24시간 모니터링'],
-  ['케어 일지', '하루 1번 케어일지 작성'],
-  ['보호자 상주', '24시간 보호자 상주'],
-  ['응급 처치', '응급 상황시 병원 이동 가능'],
+  ['소형견 케어', '7kg 미만', '/svg/serviceIcons/ServiceIcon1.png'],
+  ['중형견 케어', '7kg 이상 14.9kg 미만', '/svg/serviceIcons/ServiceIcon2.png'],
+  ['대형견 케어', '15kg 이상', '/svg/serviceIcons/ServiceIcon3.png'],
+  ['노견 케어', '8살 이상 노견 돌봄', '/svg/serviceIcons/ServiceIcon4.png'],
+  ['산책', '1일 2회 산책', '/svg/serviceIcons/ServiceIcon5.png'],
+  ['미용', '컷별 미용 가능', '/svg/serviceIcons/ServiceIcon6.png'],
+  ['목욕', '목욕 케어 가능', '/svg/serviceIcons/ServiceIcon7.png'],
+  ['실내 놀이', '터그놀이, 노즈워크 등', '/svg/serviceIcons/ServiceIcon8.png'],
+  ['CCTV', '24시간 모니터링', '/svg/serviceIcons/ServiceIcon9.png'],
+  ['케어 일지', '하루 1번 케어일지 작성', '/svg/serviceIcons/ServiceIcon10.png'],
+  ['보호자 상주', '24시간 보호자 상주', '/svg/serviceIcons/ServiceIcon11.png'],
+  ['응급 처치', '응급 상황시 병원 이동 가능', '/svg/serviceIcons/ServiceIcon12.png'],
 ]
