937d6053033265b49f94d404a7fca6f5ab80a521
penrose15 <seongju99@naver.com>
2022-10-07 15:19:47 +0900

    feat : add reservation companyName

--- server2/src/test/java/com/team012/server/ServerApplicationTests.java
+++ server2/src/test/java/com/team012/server/ServerApplicationTests.java
@@ -2,9 +2,17 @@ package com.team012.server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootTest
 class ServerApplicationTests {
+
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
 
 	@Test
 	void contextLoads() {
