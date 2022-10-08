85afbde050ef0b002fcb59b74128eed697391495
penrose15 <seongju99@naver.com>
2022-10-08 16:03:59 +0900

    invalid path

--- client/apis/type/types.ts
+++ client/apis/type/types.ts
@@ -2,40 +2,6 @@ export interface ILogin {
   email: FormDataEntryValue | null
   password: FormDataEntryValue | null
 }
-
-export interface ICustomerSignUp {
-  email: string
-  password: string
-  username: string
-  phone: string
-}
-
-export interface ICompanySingUp {
-  email: string
-  password: string
-  username: string
-  phone: string
-  ceo: string
-  companyName: string
-  address: string
-  detailAddress: string
-}
-
-export interface IPostWrite {
-  title: string
-  content: string
-  coordinate: string[]
-  checkIn: string
-  checkOut: string
-  hashTag: string[]
-  serviceTag: string[]
-  roomCount: number
-  roomDtoList: {
-    size: string
-    price: number
-  }[]
-}
-
 export interface UserSignUp {
   email: string
   password: string
@@ -45,4 +11,77 @@ export interface UserSignUp {
   address?: string
   detailAddress?: string
   ceo?: string
+}
+
+export interface IPostBookingWish {
+  map: {
+    small: number
+    medium: number
+    big: number
+  }
+  checkInDate: string
+  checkOutDate: string
+  checkInTime: string
+  checkOutTime: string
+}
+
+interface reservationListProps {
+  id: null
+  companyId: number
+  reservationDate: string
+  dogCount: number
+}
+
+interface mapProps {
+  small: number
+  medium: number
+  big: number
+}
+
+interface userInfoProps {
+  name: string
+  phone: string
+  email: string
+}
+export interface IPostBooking {
+  reservationCreateDto: {
+    reservationList: reservationListProps[]
+    dto: {
+      map: mapProps
+      checkInDate: string
+      checkOutDate: string
+      checkInTime: string
+      checkOutTime: string
+    }
+    checkInDate: string
+    checkOutDate: string
+    checkInTime: string
+    checkOutTime: string
+    totalDogCount: number
+    totalPrice: number
+  }
+  reservationUserInfoDto: {
+    dogCardsId: number[]
+    userInfo: userInfoProps
+  }
+}
+
+interface roomCreateDtoListProps {
+  size: string
+  price: number | string
+}
+export interface IPostWrite {
+  title: string
+  content: string
+  latitude: string
+  longitude: string
+  phone: string
+  address: string
+  detailAddress: string
+  checkInTime: string
+  checkOutTime: string
+  hashTag: string[]
+  serviceTag: string[]
+  roomCount: number
+  roomCreateDtoList: roomCreateDtoListProps[]
 }
